96bdb87376b62600b22f74f6c79c4365aebcef6c
pokedude9 <pokedude9@hotmail.com>
2016-06-02 19:26:59 +0200

    Created a default Qt application project.

---
+++ AwesomeMapEditor.pro
@@ -0,0 +1,63 @@
+#
+# AwesomeMapEditor: A map editor for GBA Pokémon games.
+# Copyright (C) 2016 diegoisawesome, Pokedude
+# License: General Public License 3.0
+#
+
+
+#
+# QMake Settings, 1
+#
+QT         += core gui opengl widgets
+TARGET      = AwesomeMapEditor
+TEMPLATE    = app
+CONFIG     += c++11
+DEFINES    += AME_EDITOR
+
+
+#
+# QMake Settings, 2
+#
+VERSION = 1.0
+win:QMAKE_TARGET_COMPANY     = <placeholder>
+win:QMAKE_TARGET_PRODUCT     = AwesomeMapEditor
+win:QMAKE_TARGET_DESCRIPTION = A map editor for GBA Pokémon games.
+win:QMAKE_TARGET_COPYRIGHT   = (C) 2016 diegoisawesome, Pokedude
+
+
+#
+# QMake Settings, 3
+#
+INCLUDEPATH += include
+INCLUDEPATH += include/AME/Forms
+INCLUDEPATH += $$PWD/../QBoy/include
+DEPENDPATH  += $$PWD/../QBoy/include
+
+
+#
+# QMake Settings, 4
+#
+unix|win32: LIBS += -L$$PWD/../QBoy/bin/debug/ -lQBoy
+
+
+#
+# Source Files
+#
+SOURCES += \
+        src/main.cpp \
+        src/Forms/MainWindow.cpp
+
+
+#
+# Header Files
+#
+HEADERS  += \
+        include/AME/Forms/MainWindow.h
+
+
+#
+# Forms
+#
+FORMS    += resources/UI/MainWindow.ui
+
+
---
+++ include/AME/Forms/MainWindow.h
@@ -0,0 +1,93 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+//
+//                     d88b         888b           d888  888888888888
+//                    d8888b        8888b         d8888  888
+//                   d88''88b       888'8b       d8'888  888
+//                  d88'  '88b      888 '8b     d8' 888  8888888
+//                 d88Y8888Y88b     888  '8b   d8'  888  888
+//                d88""""""""88b    888   '8b d8'   888  888
+//               d88'        '88b   888    '888'    888  888
+//              d88'          '88b  888     '8'     888  888888888888
+//
+//
+// AwesomeMapEditor: A map editor for GBA Pokémon games.
+// Copyright (C) 2016 diegoisawesome, Pokedude
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef __AME_MAINWINDOW_H__
+#define __AME_MAINWINDOW_H__
+
+
+///////////////////////////////////////////////////////////
+// Include files
+//
+///////////////////////////////////////////////////////////
+#include <QMainWindow>
+
+
+namespace ame
+{
+    namespace Ui
+    {
+        class MainWindow;
+    }
+
+
+    ///////////////////////////////////////////////////////////
+    /// \file    MainWindow.h
+    /// \author  Pokedude
+    /// \version 1.0.0.0
+    /// \date    6/2/2016
+    /// \brief   Holds components for the main AME window.
+    ///
+    ///////////////////////////////////////////////////////////
+    class MainWindow : public QMainWindow {
+    Q_OBJECT
+    public:
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Default constructor
+        /// \param parent Parental widget (optional)
+        ///
+        /// Creates all resources that are required by the GUI.
+        ///
+        ///////////////////////////////////////////////////////////
+        explicit MainWindow(QWidget *parent = 0);
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Destructor
+        ///
+        /// Releases all resources that belong to MainWindow.
+        ///
+        ///////////////////////////////////////////////////////////
+        ~MainWindow();
+
+    private:
+
+        ///////////////////////////////////////////////////////////
+        // Class members
+        //
+        ///////////////////////////////////////////////////////////
+        Ui::MainWindow *ui;    ///< Gives access to the GUI objects
+    };
+}
+
+
+#endif // __AME_MAINWINDOW_H__
