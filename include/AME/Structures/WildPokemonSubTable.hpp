3459ce6d8ebdf93f150e8bd48e427440468f9ecd
pokedude9 <mefirex@mswork.ru>
2016-06-04 09:35:29 +0200

    Added new class 'WildPokemonSubTable'.
    The sub-table holds every encounter area (grass, water, ...).

---
+++ include/AME/Structures/WildPokemonSubTable.hpp
@@ -0,0 +1,182 @@
+﻿//////////////////////////////////////////////////////////////////////////////////
+//
+//
+//                     d88b         888b           d888  888888888888
+//                    d8888b        8888b         d8888  888
+//                   d88''88b       888'8b       d8'888  888
+//                  d88'  '88b      888 '8b     d8' 888  8888888
+//                 d88Y8888Y88b     888  '8b   d8'  888  888
+//                d88""""""""88b    888   '8b d8'   888  888
+//               d88'        '88b   888    '888'    888  888
+//              d88'          '88b  888     '8'     888  888888888888
+//
+//
+// AwesomeMapEditor: A map editor for GBA Pokémon games.
+// Copyright (C) 2016 diegoisawesome, Pokedude
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef __AME_WILDPOKEMONSUBTABLE_HPP__
+#define __AME_WILDPOKEMONSUBTABLE_HPP__
+
+
+///////////////////////////////////////////////////////////
+// Include files
+//
+///////////////////////////////////////////////////////////
+#include <QBoy/Core/Rom.hpp>
+#include <AME/System/IUndoable.hpp>
+#include <AME/System/WriteEntry.hpp>
+#include <AME/Structures/WildPokemonArea.hpp>
+
+
+namespace ame
+{
+    ///////////////////////////////////////////////////////////
+    /// \brief Defines all existing encounter areas.
+    ///
+    ///////////////////////////////////////////////////////////
+    enum EncounterArea
+    {
+        EA_AREA_GRASS   = 0,    ///< Encounters in high grass
+        EA_AREA_WATER   = 1,    ///< Encounters while surfing
+        EA_AREA_ROCK    = 2,    ///< Encounters on smashing rocks
+        EA_AREA_FISH    = 3     ///< Encounters while fishing
+    };
+
+
+    ///////////////////////////////////////////////////////////
+    /// \file    WildPokemonSubTable.hpp
+    /// \author  Pokedude
+    /// \version 1.0.0.0
+    /// \date    6/3/2016
+    /// \brief   Holds the encounters for multiple areas.
+    ///
+    /// Consists of four fixed-size arrays that contain the
+    /// encounter entries for each of the areas.
+    ///
+    ///////////////////////////////////////////////////////////
+    class WildPokemonSubTable {
+    public:
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Default constructor
+        ///
+        /// Initializes a new instance of ame::WildPokemonSubTable.
+        ///
+        ///////////////////////////////////////////////////////////
+        WildPokemonSubTable();
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Copy constructor
+        ///
+        /// Copies all members of the given ame::WildPokemonSubTable.
+        /// Is only called by template code, not by actual AME code.
+        ///
+        ///////////////////////////////////////////////////////////
+        WildPokemonSubTable(const WildPokemonSubTable &rvalue);
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Assignment constructor
+        ///
+        /// Copies all members of the given ame::WildPokemonSubTable
+        /// and stores them in a new class instance.
+        ///
+        ///////////////////////////////////////////////////////////
+        WildPokemonSubTable &operator=(const WildPokemonSubTable &rvalue);
+
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Attempts to read all the area arrays.
+        ///
+        /// Each array has a fixed size and consists of several
+        /// entries that define min/max level and Pokémon ID.
+        ///
+        /// \param rom Currently opened ROM file
+        /// \param offset The offset of the sub table
+        /// \returns true on success.
+        ///
+        ///////////////////////////////////////////////////////////
+        bool read(const qboy::Rom &rom, UInt32 offset);
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Writes the area arrays to the same offset.
+        ///
+        /// As area arrays have a fixed size, they do not require
+        /// a repoint.
+        ///
+        /// \returns a list of write entries to use for the rom.
+        ///
+        ///////////////////////////////////////////////////////////
+        QList<WriteEntry> write();
+
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Retrieves the encounter array of one area.
+        /// \param area Enum value specifying the returned area
+        ///
+        /// Valid values for the first argument are:
+        /// EA_AREA_GRASS
+        /// EA_AREA_WATER
+        /// EA_AREA_ROCK
+        /// EA_AREA_FISH
+        ///
+        ///////////////////////////////////////////////////////////
+        WildPokemonArea &encounter(EncounterArea area);
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Retrieves the offset of the sub-table.
+        ///
+        ///////////////////////////////////////////////////////////
+        UInt32 offset() const;
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Retrieves the associated map bank.
+        ///
+        ///////////////////////////////////////////////////////////
+        UInt8 bank() const;
+
+        ///////////////////////////////////////////////////////////
+        /// \brief Retrieves the associated map.
+        ///
+        ///////////////////////////////////////////////////////////
+        UInt8 map() const;
+
+
+    private:
+
+        ///////////////////////////////////////////////////////////
+        // Class members
+        //
+        ///////////////////////////////////////////////////////////
+        UInt32 m_Offset;            ///< Offset of the sub-table
+        UInt8 m_Bank;               ///< Associated map bank
+        UInt8 m_Map;                ///< Associated map
+        UInt32 m_PtrGrass;          ///< Offset of the grass array
+        UInt32 m_PtrWater;          ///< Offset of the water array
+        UInt32 m_PtrRock;           ///< Offset of the rock-smash array
+        UInt32 m_PtrFish;           ///< Offset of the fishing array
+        WildPokemonArea m_AreaGrass;
+        WildPokemonArea m_AreaWater;
+        WildPokemonArea m_AreaRock;
+        WildPokemonArea m_AreaFish;
+    };
+}
+
+
+#endif // __AME_WILDPOKEMONSUBTABLE_HPP__
---
+++ src/Structures/WildPokemonSubTable.cpp
@@ -0,0 +1,256 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+//
+//                     d88b         888b           d888  888888888888
+//                    d8888b        8888b         d8888  888
+//                   d88''88b       888'8b       d8'888  888
+//                  d88'  '88b      888 '8b     d8' 888  8888888
+//                 d88Y8888Y88b     888  '8b   d8'  888  888
+//                d88""""""""88b    888   '8b d8'   888  888
+//               d88'        '88b   888    '888'    888  888
+//              d88'          '88b  888     '8'     888  888888888888
+//
+//
+// AwesomeMapEditor: A map editor for GBA Pokémon games.
+// Copyright (C) 2016 diegoisawesome, Pokedude
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+// Include files
+//
+///////////////////////////////////////////////////////////
+#include <AME/Structures/StructureErrors.hpp>
+#include <AME/Structures/WildPokemonSubTable.hpp>
+
+
+namespace ame
+{
+    ///////////////////////////////////////////////////////////
+    // Function type:  Constructor
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/3/2016
+    //
+    ///////////////////////////////////////////////////////////
+    WildPokemonSubTable::WildPokemonSubTable()
+        : m_Offset(0),
+          m_Bank(0),
+          m_Map(0),
+          m_PtrGrass(0),
+          m_PtrWater(0),
+          m_PtrRock(0),
+          m_PtrFish(0)
+    {
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Function type:  Constructor
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/3/2016
+    //
+    ///////////////////////////////////////////////////////////
+    WildPokemonSubTable::WildPokemonSubTable(const WildPokemonSubTable &rvalue)
+        : m_Offset(rvalue.m_Offset),
+          m_Bank(rvalue.m_Bank),
+          m_Map(rvalue.m_Map),
+          m_PtrGrass(rvalue.m_PtrGrass),
+          m_PtrWater(rvalue.m_PtrWater),
+          m_PtrRock(rvalue.m_PtrRock),
+          m_PtrFish(rvalue.m_PtrFish)
+    {
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Function type:  Constructor
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/3/2016
+    //
+    ///////////////////////////////////////////////////////////
+    WildPokemonSubTable &WildPokemonSubTable::operator=(const WildPokemonSubTable &rvalue)
+    {
+        m_Offset = rvalue.m_Offset;
+        m_Bank = rvalue.m_Bank;
+        m_Map = rvalue.m_Map;
+        m_PtrGrass = rvalue.m_PtrGrass;
+        m_PtrWater = rvalue.m_PtrWater;
+        m_PtrRock = rvalue.m_PtrRock;
+        m_PtrFish = rvalue.m_PtrFish;
+        return *this;
+    }
+
+
+    ///////////////////////////////////////////////////////////
+    // Function type:  I/O
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/3/2016
+    //
+    ///////////////////////////////////////////////////////////
+    bool WildPokemonSubTable::read(const qboy::Rom &rom, UInt32 offset)
+    {
+        if (!rom.seek(offset))
+            AME_THROW(WPST_ERROR_OFFSET, rom.redirected());
+
+        // Fetches the corresponding map index at a specific bank
+        m_Bank = rom.readByte();
+        m_Map = rom.readByte();
+        rom.readHWord(); /* Padding */
+
+
+        // Reads all pointers to each area
+        m_PtrGrass = rom.readPointer();
+        m_PtrWater = rom.readPointer();
+        m_PtrRock = rom.readPointer();
+        m_PtrFish = rom.readPointer();
+
+
+        // Determines whether all read pointers are valid
+        if (!rom.checkOffset(m_PtrGrass))
+            AME_THROW(WPST_ERROR_GRASS, offset + 4);
+        if (!rom.checkOffset(m_PtrWater))
+            AME_THROW(WPST_ERROR_WATER, offset + 8);
+        if (!rom.checkOffset(m_PtrRock))
+            AME_THROW(WPST_ERROR_ROCK, offset + 12);
+        if (!rom.checkOffset(m_PtrFish))
+            AME_THROW(WPST_ERROR_FISH, offset + 16);
+
+
+        // Defines objects for reading the encounter tables dynamically
+        WildPokemonArea* areas[] { &m_AreaGrass, &m_AreaWater, &m_AreaRock, &m_AreaFish };
+        UInt32 pointers[] { m_PtrGrass, m_PtrWater, m_PtrRock, m_PtrFish };
+        UInt32 entrycnt[] { 12, 5, 5, 10 };
+
+
+        // Reads each encounter table dynamically
+        for (int i = 0; i < 4; i++)
+        {
+            WildPokemonArea *currentArea = areas[i];
+            UInt32 currentOffset = pointers[i];
+            UInt32 currentAmount = entrycnt[i];
+
+            // If pointer is a NULL pointer, abort reading
+            if (!currentOffset)
+                continue;
+
+            // Reads the current area
+            if (!currentArea->read(rom, currentOffset, currentAmount))
+                return false;
+        }
+
+
+        // Loading successful
+        return true;
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Function type:  I/O
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/4/2016
+    //
+    ///////////////////////////////////////////////////////////
+    QList<WriteEntry> WildPokemonSubTable::write()
+    {
+        QList<WriteEntry> entries;
+        WriteEntry entry;
+        entry.offset = m_Offset;
+
+
+        // Adds bank, map and all the pointers to the entry
+        entry.data.push_back(m_Bank);
+        entry.data.push_back(m_Map);
+        entry.addHWord(0x0000); // padding
+        entry.addPointer(m_PtrGrass);
+        entry.addPointer(m_PtrWater);
+        entry.addPointer(m_PtrRock);
+        entry.addPointer(m_PtrFish);
+        entries.push_back(entry);
+
+        // Writes each encounter array (if their offset is not NULL)
+        if (m_PtrGrass)
+            entries.append(m_AreaGrass.write(m_PtrGrass));
+        if (m_PtrWater)
+            entries.append(m_AreaWater.write(m_PtrWater));
+        if (m_PtrRock)
+            entries.append(m_AreaRock.write(m_PtrRock));
+        if (m_PtrFish)
+            entries.append(m_AreaFish.write(m_PtrFish));
+
+
+        return entries;
+    }
+
+
+    ///////////////////////////////////////////////////////////
+    // Function type:  Getter
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/3/2016
+    //
+    ///////////////////////////////////////////////////////////
+    WildPokemonArea &WildPokemonSubTable::encounter(EncounterArea area)
+    {
+        switch (area)
+        {
+            case EA_AREA_GRASS: return m_AreaGrass;
+            case EA_AREA_WATER: return m_AreaWater;
+            case EA_AREA_ROCK:  return m_AreaRock;
+            case EA_AREA_FISH:  return m_AreaFish;
+            default:            return m_AreaGrass;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Function type:  Getter
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/4/2016
+    //
+    ///////////////////////////////////////////////////////////
+    UInt32 WildPokemonSubTable::offset() const
+    {
+        return m_Offset;
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Function type:  Getter
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/4/2016
+    //
+    ///////////////////////////////////////////////////////////
+    UInt8 WildPokemonSubTable::bank() const
+    {
+        return m_Bank;
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Function type:  Getter
+    // Contributers:   Pokedude
+    // Last edit by:   Pokedude
+    // Date of edit:   6/4/2016
+    //
+    ///////////////////////////////////////////////////////////
+    UInt8 WildPokemonSubTable::map() const
+    {
+        return m_Map;
+    }
+}
